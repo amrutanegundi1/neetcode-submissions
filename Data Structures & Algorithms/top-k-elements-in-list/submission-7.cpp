38490ac999f0481b94b2d03f723212b0ac76fb4c
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-05-26 14:10:58 +0530

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,30 @@
+struct Customcomp
+    {
+        bool operator()(const pair<int, int> &a, const pair<int, int> &b)
+        {
+            return a.first < b.first;
+        }
+    };
+class Solution {
+    
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> counter;
+        for(int i = 0; i<nums.size();i++)
+        {
+            counter[nums[i]]++;
+        }
+        
+        priority_queue<pair<int, int>> heap;
+        for (auto& entry : counter) {
+            heap.push(pair<int,int>(entry.second, entry.first));
+        }
+
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
+    }
+};
