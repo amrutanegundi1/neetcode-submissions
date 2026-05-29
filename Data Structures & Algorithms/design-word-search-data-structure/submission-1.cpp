ff816cb56611377c3008e94b991b719dd875b088
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-05-29 10:59:21 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,74 @@
+class TrieNode
+{
+    public:
+    unordered_map<char, TrieNode*> children;
+    bool endOfWord;
+};
+class WordDictionary {
+    TrieNode * root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode *temp = root;
+        for(int i = 0; i < word.length(); i++)
+        {
+            if(temp->children[word[i]] == nullptr)
+            {
+                temp->children[word[i]] = new TrieNode();
+            }
+            temp = temp->children[word[i]];
+        }
+        temp->endOfWord = true; 
+    }
+    
+    bool search(string word) {
+        queue<TrieNode*> nodesToSearch;
+        
+        nodesToSearch.push(root);
+       
+        for(int i = 0; i < word.length(); i++)
+        {
+            if(word[i] == '.')
+            {
+                for(int j = nodesToSearch.size()-1; j >= 0; j--)
+                {
+                    TrieNode *curr = nodesToSearch.front();
+                    nodesToSearch.pop();
+                    for(auto itr : curr->children)
+                    {
+                        nodesToSearch.push(itr.second);
+                    }
+                }
+            }
+            else 
+            {
+                for(int j = nodesToSearch.size()-1; j >= 0; j--)
+                {
+                    TrieNode *curr = nodesToSearch.front();
+                    nodesToSearch.pop();
+                    if(curr->children.find(word[i]) != curr->children.end())
+                    {
+                        nodesToSearch.push(curr->children[word[i]]);
+                    }
+                }
+            }
+            if(nodesToSearch.size() == 0)
+            {
+                break;
+            }
+        }
+        if(nodesToSearch.size() > 0)
+        {
+            TrieNode *curr = nodesToSearch.front();
+            nodesToSearch.pop();
+            if(curr->endOfWord)
+            {
+                return true;
+            }
+        }
+        return false;
+     }
+};
