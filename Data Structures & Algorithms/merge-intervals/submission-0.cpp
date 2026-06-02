4a8b151924f7c23e5a961dd90f8cdddc17a1e027
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-02 13:00:57 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+
+        vector<int> prev = intervals[0];
+
+        for(int i = 1; i < intervals.size(); i++)
+        {
+            // if the prev is before the current add prev and point curr to prev
+            if(prev[1] < intervals[i][0])
+            {
+                res.push_back(prev);
+                prev = intervals[i];
+            }
+            else
+            {
+                // this is an overlap
+                prev[0] = min(prev[0], intervals[i][0]);
+                prev[1] = max(prev[1], intervals[i][1]);
+            }
+        }
+        res.push_back(prev);
+        return res;
+    }
+};
