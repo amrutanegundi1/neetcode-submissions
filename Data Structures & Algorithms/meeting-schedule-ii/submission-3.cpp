8a384b565accf2dcb8d0f13058e8a7e97a9c0c79
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-02 17:50:39 +0530

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+
+public:
+    static bool comp(const Interval &a, const Interval &b)
+    {
+        return a.start < b.start;
+    }
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> rooms; 
+        if(intervals.size() == 0)
+            return 0;
+        sort(intervals.begin(), intervals.end(), comp);
+        rooms.push_back(intervals[0].end);
+        cout<<rooms[0]<<endl;
+        for(int i = 1; i < intervals.size(); i++)
+        {
+            bool roomFound = false;
+            for(int j = 0; j < rooms.size(); j++)
+            {
+                if(rooms[j] <= intervals[i].start)
+                {
+                    roomFound = true;
+                    rooms[j] = intervals[i].end;
+                    break;
+                }
+            }
+            if(!roomFound)
+            {
+                rooms.push_back(intervals[i].end);
+            }
+        }
+        return rooms.size();
+        
+    }
+};
