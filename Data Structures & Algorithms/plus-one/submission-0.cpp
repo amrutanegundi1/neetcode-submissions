9509f7d6f069baa55c46812403bb64d734f2e1d5
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-07 11:08:05 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    vector<int> plusOne(vector<int>& digits) {
+        bool carry = true;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (!carry) break;
+            int res = digits[i] + 1;
+            if (res == 10) {
+                carry = true;
+                digits[i] = 0;
+            } else {
+                carry = false;
+                digits[i] = res;
+            }
+        }
+        if(carry)
+        {
+            digits.push_back(0);
+            for(int i = digits.size()-2; i >= 0; i--)
+            {
+                digits[i+1] = digits[i];
+            }
+            digits[0] = 1;
+        }
+        return digits;
+    }
+};
