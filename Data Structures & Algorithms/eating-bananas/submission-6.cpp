2e240e995497b6ec0b15673452db023cbf5fa4a4
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-08 15:40:34 +0530

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int max = INT_MIN;
+        for (auto itr : piles) {
+            if(itr > max)
+                max = itr;
+        }
+        int l = 1, r = max;
+
+        int res = INT_MAX;
+
+        while(l <= r)
+        {
+            int mid = l + (r-l)/2;
+            long hr = 0;
+            for(auto itr : piles)
+                hr += ceil(double(itr)/mid); 
+            if(hr <= h)
+            {
+                if(res > mid)
+                    res = mid;
+                r = mid-1;
+            }
+            else if(hr > h)
+            {
+                l = mid +1;
+            }
+        }
+        return res;
+    }
+};
