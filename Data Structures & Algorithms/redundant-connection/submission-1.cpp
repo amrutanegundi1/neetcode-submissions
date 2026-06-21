0441343a7d430ee20192d3a02c254c6627b4233a
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-21 12:25:07 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<vector<int>> adj(edges.size()+1);
+
+        for(auto itr : edges)
+        {
+            adj[itr[0]].push_back(itr[1]);
+            adj[itr[1]].push_back(itr[0]);
+            unordered_set<int> visited;
+            if(dfs(itr[0], -1, visited, adj))
+            {
+                return itr;
+            }
+
+        }
+        return vector<int>{};      
+    }
+
+    bool dfs(int node, int parent, unordered_set<int> visited, vector<vector<int>> &adj)
+    {
+        if(visited.find(node) != visited.end())
+        {
+            return true;
+        }
+
+        visited.insert(node);
+
+        for(auto itr : adj[node])
+        {
+            if(itr == parent)
+            {
+                continue;
+            }
+            else if(dfs(itr, node, visited, adj))
+            {
+                return true;
+            }
+        }
+        visited.erase(node);
+        return false;
+    }
+
+};
