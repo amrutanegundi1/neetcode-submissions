7734229ad8dfcce6add2155ea604a786bebaf1ce
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-22 18:23:38 +0530

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.cpp
@@ -0,0 +1,60 @@
+class Solution {
+   public:
+    map<pair<int, int>, int> mp;
+    int minDistance(string word1, string word2) {
+        // return dfs(word1, word2, 0, 0);
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+
+        for (int i = 0; i <= word1.size(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.size(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j++) {
+                if(word1[i-1] == word2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+                }
+            }
+        }
+        // for (int i = 0; i <= word1.size(); i++) {
+        //     for (int j = 0; j <= word2.size(); j++) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[word1.size()][word2.size()];
+    }
+
+    int dfs(string word1, string word2, int i1, int j1) {
+        pair<int, int> pr = pair<int, int>(i1, j1);
+        if (mp.find(pr) != mp.end()) {
+            return mp[pr];
+        }
+        if (i1 == word1.length() && j1 == word2.length()) {
+            return 0;
+        }
+        if (i1 == word1.length() && j1 != word2.length()) {
+            return word2.length() - j1;
+        }
+        if (j1 == word2.length() && i1 != word1.length()) {
+            return word1.length() - i1;
+        }
+        if (word1[i1] == word2[j1]) {
+            return dfs(word1, word2, i1 + 1, j1 + 1);
+        } else {
+            mp[pr] = min(/* insert*/ dfs(word1, word2, i1, j1 + 1),
+                         min(/*delete*/ dfs(word1, word2, i1 + 1, j1),
+                             dfs(word1, word2, i1 + 1, j1 + 1))) +
+                     1;
+            return mp[pr];
+        }
+    }
+};
