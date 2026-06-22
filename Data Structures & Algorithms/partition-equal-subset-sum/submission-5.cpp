26580ee519be1948a0025494eead605658cd4876
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-22 10:43:01 +0530

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int total = 0;
+        for(auto itr : nums)
+        {
+            total += itr;
+        }
+        if(total % 2 != 0)
+        {
+            return false;
+        }
+        total = total/2;
+        set<int> s;
+        s.insert(0);
+        for(int i = 0; i < nums.size(); i++)
+        {
+            set<int> temp = s;
+            for(auto itr : s)
+            {
+                if(itr + nums[i] == total)
+                {
+                    return true;
+                }
+                temp.insert(itr + nums[i]);
+            }
+            s = temp;
+        }
+        if(s.find(total) != s.end())
+            return true;
+        return false;
+    }
+};
