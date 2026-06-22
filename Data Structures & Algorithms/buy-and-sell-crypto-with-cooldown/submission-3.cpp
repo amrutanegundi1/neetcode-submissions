290ded61c9d998cab6a84a2be6b9b515b35ad583
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-06-22 11:28:17 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    map<pair<int, bool>, int> mp;
+    int maxProfit(vector<int>& prices) { return dfs(0, true, prices); }
+    int dfs(int i, bool buying, vector<int>& prices) {
+        if (i >= prices.size()) {
+            return 0;
+        }
+        pair<int, bool> pr = pair<int, bool>(i, buying);
+        if (mp.find(pr) != mp.end()) {
+            return mp[pr];
+        }
+        int cooldown = dfs(i + 1, buying, prices);
+        if (buying) {
+            int buy = dfs(i + 1, false, prices) - prices[i];
+
+            mp[pr] = max(buy, cooldown);
+        } else {
+            int sell = dfs(i + 2, true, prices) + prices[i];
+            mp[pr] = max(sell, cooldown);
+        }
+        return mp[pr];
+    }
+};
