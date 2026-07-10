41ba5ef9b117b0511ca071826d5a3618c2364ac2
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-10 18:44:30 +0530

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.cpp
@@ -0,0 +1,87 @@
+class TrieNode {
+   public:
+    unordered_map<char, TrieNode*> children;
+    bool endOfWord;
+};
+class WordDictionary {
+    TrieNode* root;
+
+   public:
+    WordDictionary() { root = new TrieNode(); }
+
+    void addWord(string word) {
+        TrieNode* temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (temp->children[word[i]] == nullptr) {
+                temp->children[word[i]] = new TrieNode();
+            }
+            temp = temp->children[word[i]];
+        }
+        temp->endOfWord = true;
+    }
+
+    bool search(string word) {
+        return dfs(word, root, 0);
+        // queue<TrieNode*> nodesToSearch;
+
+        // nodesToSearch.push(root);
+
+        // for (int i = 0; i < word.length(); i++) {
+        //     if (word[i] == '.') {
+        //         for (int j = nodesToSearch.size() - 1; j >= 0; j--) {
+        //             TrieNode* curr = nodesToSearch.front();
+        //             nodesToSearch.pop();
+        //             for (auto itr : curr->children) {
+        //                 nodesToSearch.push(itr.second);
+        //             }
+        //         }
+        //     } else {
+        //         for (int j = nodesToSearch.size() - 1; j >= 0; j--) {
+        //             TrieNode* curr = nodesToSearch.front();
+        //             nodesToSearch.pop();
+        //             if (curr->children.find(word[i]) != curr->children.end()) {
+        //                 nodesToSearch.push(curr->children[word[i]]);
+        //             }
+        //         }
+        //     }
+        //     if (nodesToSearch.size() == 0) {
+        //         break;
+        //     }
+        // }
+        // if (nodesToSearch.size() > 0) {
+        //     TrieNode* curr = nodesToSearch.front();
+        //     nodesToSearch.pop();
+        //     if (curr->endOfWord) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+    }
+
+    bool dfs(string word, TrieNode* root, int ind) {
+
+        TrieNode* curr = root;
+        if(ind == word.length())
+        {
+            return curr->endOfWord;
+        }
+
+        if (word[ind] == '.') {
+            for (auto itr : curr->children) {
+                if (dfs(word, itr.second, ind + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            if (curr->children.find(word[ind]) == curr->children.end()) {
+                return false;
+            }
+            else
+            {
+                return dfs(word, curr->children[word[ind]], ind + 1);
+            }
+        }
+        return curr->endOfWord;
+    }
+};
