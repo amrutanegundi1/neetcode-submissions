ee5731900cec1a7f0ca153ab20d34a89965ac165
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-12 18:14:53 +0530

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int res = dfs(coins,amount,0);
+        if(res == INT_MAX)
+            return -1;
+        return res;
+    }
+
+    int dfs(vector<int>& coins, int amount, int ind)
+    {
+        if(ind >= coins.size() || amount < 0)
+        {
+            return INT_MAX;
+        }
+        if(amount == 0)
+        {
+            return 0;
+        }
+        int res1 = dfs(coins, amount - coins[ind], ind);
+        int res2 = dfs(coins, amount, ind+1);
+        if(res1 == INT_MAX)
+            return res2;
+        return min((1 + res1), res2);
+    }
+};
