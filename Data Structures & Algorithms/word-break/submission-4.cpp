78c3150a6cfb209bd4eae5c9bd594eb4b35bfe9e
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-12 20:13:53 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.length() + 1, false);
+        dp[s.length()] = true;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (string itr : wordDict) {
+                if (i + itr.length() <= s.length()) {
+                    if (s.substr(i, itr.length()) == itr) {
+                        dp[i] = dp[i + itr.length()];
+                        if (dp[i] == true) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0];
+    }
+};
