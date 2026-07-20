e27636e52eb046c59e0e61332ea1446b249f88bb
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-20 21:54:56 +0530

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            int key = nums[i];
+            int j = i -1;
+            while(j >= 0 && nums[j] > key)
+            {
+                nums[j+1] = nums[j];
+                j = j-1;
+            }
+            nums[j+1] = key;
+        }
+        return nums;
+        
+    }
+};
