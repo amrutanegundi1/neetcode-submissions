c938e0f1fd1733dadaa5d508c3878d9585e859a1
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-20 21:32:08 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            int id = i;
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                if(nums[j] < nums[id])
+                {
+                    id = j;
+                }
+            }
+            swap(nums[i],nums[id]);
+        }
+        return nums;
+        
+    }
+};
