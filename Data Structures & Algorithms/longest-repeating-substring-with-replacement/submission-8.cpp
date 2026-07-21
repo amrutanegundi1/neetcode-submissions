c775c240b3d69167737132bb554abbdbc061e1eb
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-21 21:10:36 +0530

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+       
+        int max_count = 0;
+        int l = 0;
+        array<int, 26> counter = {0};
+
+        for(int r =0; r < s.length();r++)
+        {
+            counter[s[r] - 'A']++;
+            if(r - l +1 - k <= maxFreq(counter))
+            {
+                max_count = max(max_count, r - l +1);
+                continue;
+            }
+            else
+            {
+                counter[s[l] - 'A']--;
+                l++;
+            }
+        }
+        return max_count;
+    }
+
+    int maxFreq(array<int, 26> counter)
+    {
+        int maxCount = 0;
+        for( int i =0; i< 26; i++)
+        {
+            maxCount = max(maxCount, counter[i]);
+        }
+        return maxCount;
+    }
+};
