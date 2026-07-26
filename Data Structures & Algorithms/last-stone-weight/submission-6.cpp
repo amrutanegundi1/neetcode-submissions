2003481ab1ff54bdf1039db90c392654fd0dfb2a
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-26 16:51:07 +0530

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> st;
+
+        for(auto itr : stones)
+        {
+            st.push(itr);
+        }
+        int counter = 0;
+        while(st.size() > 1)
+        {
+            int t1 = st.top();
+            st.pop();
+            int t2 = st.top();
+            st.pop();
+            if(t1 == t2)
+            {
+                ;
+            }
+            else {
+                st.push(abs(t1-t2));
+            }
+        }
+        if(st.size() == 0)
+        {
+            return 0;
+        }
+        return st.top();
+
+    }
+};
