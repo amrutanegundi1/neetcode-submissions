de573c965634f77c5365947e6d4fffad4508d776
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-27 13:18:46 +0530

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.cpp
@@ -0,0 +1,67 @@
+class DSU
+{
+    public:
+        vector<int> parent;
+        vector<int> rank;
+        DSU(int n)
+        {
+            rank.resize(n, 1);
+            parent.resize(n);
+            for(int i = 0; i < n; i++)
+            {
+                parent[i] = i;
+            }
+        }
+
+        int findSet(int node)
+        {
+            int curr = node;
+
+            while(parent[curr] != curr)
+            {
+                curr = parent[curr];
+                parent[curr] = parent[parent[curr]];
+            }
+            return curr;
+        }
+
+        int unionSet(int n1, int n2)
+        {
+            int p1 = findSet(n1);
+            int p2 = findSet(n2);
+
+            if(p1 == p2)
+            {
+                return 0;
+            }
+
+            if(rank[p1] > rank[p2])
+            {
+                parent[p2] = p1;
+                rank[p1] += rank[p2]; 
+            }
+            else
+            {
+                parent[p1] = p2;
+                rank[p2] += rank[p1];
+            }
+            return 1;
+        }
+};
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+
+        int res = n;
+        DSU dsu(n);
+
+        for(auto itr : edges)
+        {
+            if(dsu.unionSet(itr[0], itr[1]) == 1)
+            {
+                res--;
+            }
+        }
+        return res;
+    }
+};
