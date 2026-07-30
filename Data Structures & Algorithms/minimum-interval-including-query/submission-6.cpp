44f103f17e2437f293492de36665644c23c4b35b
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-30 20:10:19 +0530

    Add: minimum-interval-including-query - submission-6

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-6.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < intervals.size(); i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int interval = end - start + 1;
+            for(int j = start; j <= end; j++)
+            {
+                if(mp.find(j) != mp.end())
+                {
+                    mp[j] = min(mp[j], interval);
+                }
+                else
+                {
+                    mp[j] = interval;
+                }
+            }
+        }
+        vector<int> res;
+        for(int i = 0; i < queries.size(); i++)
+        {
+            if(mp.find(queries[i]) != mp.end())
+            {
+                res.push_back(mp[queries[i]]);
+            }
+            else
+            {
+                res.push_back(-1);
+            }
+        }
+        return res;
+        
+    }
+};
