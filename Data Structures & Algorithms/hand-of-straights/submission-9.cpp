a674a22e7493a620468ae3a700805e98e3713b11
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-07-30 17:27:14 +0530

    Add: hand-of-straights - submission-9

---
+++ Data Structures & Algorithms/hand-of-straights/submission-9.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        unordered_map<int, int> mp;
+
+        for (auto itr : hand) {
+            mp[itr]++;
+        }
+        if (hand.size() % groupSize != 0) return false;
+        sort(hand.begin(),hand.end());
+        for (int i = 0; i < hand.size(); i++) {
+            if (mp.size() != 0 && mp.find(hand[i]) != mp.end()) {
+                int counter = 0;
+                int start = hand[i]-1;
+                while (counter < groupSize) {
+                    
+                    start++;
+                    cout<<start<<endl;
+                    if(mp.find(start) == mp.end())
+                    {
+                        return false;
+                    }
+                    mp[start]--;
+                    if (mp[start] == 0) mp.erase(start);
+                    counter++;
+                }
+            }
+        }
+        return true;
+    }
+};
