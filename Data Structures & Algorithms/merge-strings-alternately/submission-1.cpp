27b0129e985577344a37a9cbf45294b5e25fd621
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-08-11 20:00:07 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int l1 = 0, l2 = 0;
+        int s1 = word1.length(), s2 = word2.length();
+        string res;
+        while(l1 < s1 && l2 < s2)
+        {
+            res += word1[l1++];
+            res += word2[l2++];        
+        }
+        if(s1 < s2)
+        {
+            res += word2.substr(l1);
+        }
+        else if(s2 < s1)
+        {
+            res += word1.substr(l2);
+        }
+        return res;
+    }
+};
