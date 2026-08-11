89fd87f09d5f1783f58cfc1ac7b6d67d0ddf993e
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-08-11 12:39:50 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-5

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    map<pair<int, bool>, int> mp;
+    int res = 0;
+    int maxProfit(vector<int>& prices) {
+        dfs(prices, true, 0);
+        return res;
+    }
+    int dfs(vector<int>& prices, bool buy, int i) {
+        if (i >= prices.size()) {
+            return 0;
+        }
+
+        if (mp.find(pair<int, bool>(i, buy)) != mp.end()) {
+            return mp[pair<int, bool>(i, buy)];
+        }
+
+        // do not do anything today
+        int res1 = dfs(prices, buy, i + 1);
+        int res2 = 0, res3 = 0;
+        if (buy) {
+            res2 = dfs(prices, false, i + 1) - prices[i];
+
+            res = max(res, max(res1, res2));
+        } else {
+            res3 = dfs(prices, true, i + 1) + prices[i];
+        }
+        mp[pair<int, bool>(i, buy)] = max(res1, max(res2, res3));
+        return mp[pair<int, bool>(i, buy)];
+    }
+};
