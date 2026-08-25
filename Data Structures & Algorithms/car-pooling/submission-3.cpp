eb3e0ee9011a4f91faef0ca9c7960aafca14ee09
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-08-25 18:34:15 +0530

    Add: car-pooling - submission-3

---
+++ Data Structures & Algorithms/car-pooling/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < trips.size(); i++)
+        {
+            for(int j = trips[i][1]; j < trips[i][2]; j++)
+            {
+                if(mp.find(j) == mp.end())
+                {
+                    mp[j] = capacity - trips[i][0];
+                }
+                else
+                {
+                    mp[j] = mp[j] - trips[i][0];
+                }
+                if(mp[j] < 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
