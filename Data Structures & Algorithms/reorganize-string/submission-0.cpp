f0f1b67e761a5ffb7c3d8ab1c36be9d9cc20480a
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-08-25 16:46:51 +0530

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> mp;
+        for(auto itr : s)
+        {
+            mp[itr]++;
+        }
+        priority_queue<pair<int, char>> que;
+        for(auto itr : mp)
+        {
+            que.push({itr.second, itr.first});
+        }
+        string result = "";
+        bool toPush = false;
+        pair<int, char> pp;
+        while(!que.empty())
+        {
+            pair<int, char> pr = que.top();
+            que.pop();
+            result += pr.second;
+            if(que.empty() && toPush == false && pr.first > 1)
+            {
+                return "";
+            }
+            if(toPush)
+            {
+                toPush = false;
+                que.push(pp);
+            }
+            if(pr.first > 1)
+            {
+                toPush = true;
+                pp = {pr.first-1, pr.second};
+            }
+        }
+        return result;
+        
+    }
+};
