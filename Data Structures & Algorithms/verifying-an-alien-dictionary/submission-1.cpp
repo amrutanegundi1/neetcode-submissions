8b22be4d57328db502c21de0b8ad9a109dc27c74
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-08-27 19:18:19 +0530

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> mp;
+        for (int i = 0; i < order.length(); i++) {
+            mp[order[i]] = i;
+        }
+        for (int i = 0; i < words.size() - 1; i++) {
+            string w1 = words[i], w2 = words[i + 1];
+
+            for (int j = 0; j < w1.length(); j++) {
+                if (j == w2.length()) {
+                    return false;
+                }
+                if (w1[j] != w2[j]) {
+                    if (mp[w1[j]] > mp[w2[j]]) {
+                        return false;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
