25da416406e08975801aa31d3ae799a2d24743c4
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-09-03 12:37:20 +0530

    Add: maximum-sum-circular-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int globalmin = nums[0], globalmax = nums[0], currmin = 0, currmax = 0, total = 0;
+
+        for(auto itr : nums)
+        {
+            currmax += itr;
+            currmax = max(itr, currmax);
+            globalmax = max(globalmax, currmax);
+            currmin += itr;
+            currmin = min(itr, currmin);
+            globalmin = min(globalmin, currmin);
+            total += itr;
+        }
+        cout<<globalmax<<" "<<globalmin<<endl;
+        if(globalmax < 0)
+        {
+            return globalmax;
+        }
+        return max(globalmax, total - globalmin);
+    }
+};
