590716e5355e723423757d8dd34d0d7a0b84f308
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-09-03 11:23:06 +0530

    Add: stone-game-iii - submission-3

---
+++ Data Structures & Algorithms/stone-game-iii/submission-3.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    map<pair<int, bool>, int> mp; 
+    string stoneGameIII(vector<int>& stoneValue) {
+        int alicesum = dfs(stoneValue, 0, true);
+        int total = accumulate(stoneValue.begin(), stoneValue.end(), 0);
+        int bobsum = total - alicesum;
+        if(alicesum > bobsum)
+        {
+            return "Alice";
+        }
+        else if(alicesum < bobsum)
+        {
+            return "Bob";
+        }
+        return "Tie";
+    }
+
+    int dfs(vector<int>& stoneValue, int i, bool alice)
+    {
+        if(i >= stoneValue.size())
+        {
+            return 0;
+        }
+        if(mp.find({i, alice}) != mp.end())
+        {
+            return mp[{i, alice}];
+        }
+        int res = INT_MIN;
+        if(!alice)
+        {
+            res = INT_MAX;
+        }
+        for(int j = 0; j < 3; j++)
+        {
+            int sum = 0;
+            
+            for(int k = 0; k <= j; k++)
+            {
+                if(i + k >= stoneValue.size())
+                {
+                    break;
+                }
+                sum += stoneValue[i + k];
+                //cout<<sum<<endl;
+            }
+
+            if(alice)
+            {
+                res = max(res, sum + dfs(stoneValue, i + j + 1, !alice));
+            }
+            else
+            {
+                res = min(res, dfs(stoneValue, i + j + 1, !alice));
+            }
+        }
+        mp[{i, alice}] = res;
+        return res;
+    }
+};
