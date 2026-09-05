ab092e275d13ae36117d3c48d8f9957ff76ef0ae
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-09-05 09:59:58 +0530

    Add: insert-greatest-common-divisors-in-linked-list - submission-1

---
+++ Data Structures & Algorithms/insert-greatest-common-divisors-in-linked-list/submission-1.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* curr, *prev = nullptr;
+        curr = head;
+
+        while(prev == nullptr)
+        {
+            prev = curr;
+            curr = curr->next;
+            if(!curr)
+            {
+                break;
+            }
+            int gcd = gCD(prev->val, curr->val);
+            ListNode *temp = new ListNode(gcd);
+            prev->next = temp;
+            temp->next = curr;
+            prev = nullptr;
+        }
+        return head;
+    }
+    int gCD(int n1, int n2)
+    {
+        for(int i = min(n1, n2); i > 0; i--)
+        {
+            if(n1 % i == 0 && n2 % i == 0)
+            {
+                return i;
+            }
+        }
+        return 1;
+    }
+};
