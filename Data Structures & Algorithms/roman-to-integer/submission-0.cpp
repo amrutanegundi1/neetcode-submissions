71262b195eab3711227353feabe1c4c8bb1a6fb6
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-09-05 10:50:25 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if((s[i] == 'I' || s[i] == 'X' || s[i] == 'C') && i + 1 < s.length())
+            {
+                int temp = res;
+                if(s[i] == 'I')
+                {
+                    if(s[i+1] == 'V')
+                        res += 4;
+                    if(s[i+1] == 'X')
+                        res += 9;
+                }
+                else if(s[i] == 'X')
+                {
+                    if(s[i+1] == 'L')
+                        res += 40;
+                    if(s[i+1] == 'C')
+                        res += 90;
+                }
+                else if(s[i] == 'C')
+                {
+                    if(s[i+1] == 'D')
+                        res += 400;
+                    if(s[i+1] == 'M')
+                        res += 900;
+                }
+                if(temp != res)
+                {
+                    i = i + 1;
+                    continue;
+                }
+                res += romanToVal(s[i]);
+            }
+            else
+            {
+                res += romanToVal(s[i]);
+            }
+        }
+        return res;
+    }
+    int romanToVal(char c)
+    {
+        switch(c)
+        {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+};
