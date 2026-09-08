246fcbbdcd74a3adccc131f6a6eaa5c2af1c78ae
amrutanegundi1 <94067384+amrutanegundi1@users.noreply.github.com>
2026-09-08 17:27:55 +0530

    Add: minimum-array-end - submission-0

---
+++ Data Structures & Algorithms/minimum-array-end/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+
+        long long res = x;
+
+        for(int i = 0; i < n-1; i++)
+        {
+            res = res + 1;
+            res = res | x;
+        }
+        return res;
+    }
+};
